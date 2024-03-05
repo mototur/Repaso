c3364f0fe52dcd1b6e3d50a1237ad0627a432403
Lucia Sandoval <asandovalp9@miumg.edu.gt>
2024-03-04 21:30:34 -0600

    Commit repaso
    Gracias

---
+++ REPASO/REPASO/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REPASO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese primer ingreso");
+                int mesUno = int.Parse(Console.ReadLine());
+               
+            Console.ReadKey();
+
+
+        }
+    }
+}
